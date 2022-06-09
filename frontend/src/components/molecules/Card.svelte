2812fd64356b230486cf3dadf50cc1f280ff1066
izakikazuho <k_izaki@cinra.net>
2022-06-10 04:19:20 +0900

    add frontend

---
+++ frontend/src/components/molecules/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let data;
+</script>
+
+
+<a href="{data.url}" class="h-full">
+  <article class="border border-current hover:bg-purple-50 h-full">
+    <figure>
+      <img class="aspect-[3/2]" src={data.thumbnail} alt="">
+    </figure>
+    <h1 class="px-4 py-3 text-md italic font-semibold">
+      {data.title}
+    </h1>
+  </article>
+</a>
